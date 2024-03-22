765a2629ad5c31e4fd3c5cabddd83aeb20db8d40
Cadar Maxamed <cadarmaxamed@Cadars-MacBook-Air.local>
2024-03-22 10:56:34 -0700

    added the second routes

---
+++ routes/jobPostings.mjs
@@ -0,0 +1,57 @@
+import express from 'express';
+import db from '../db/conn.mjs';
+import { ObjectId } from 'mongodb';
+
+const router = express.Router();
+
+// Get all East African countries (checked)
+router.get('/', async (req, res) => {
+  let collection = await db.collection('job_postings');
+  let result = await collection.find({}).limit(10).toArray();
+  res.send(result);
+});
+
+// Get a single East African country by ID (checked)
+router.get('/:id', async (req, res) => {
+  let collection = await db.collection('job_postings');
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
+
+  if (!result) res.status(404).send('Not found');
+  else res.status(200).send(200);
+});
+
+// Create a new East African country (Checked!!!)
+router.post('/', async (req, res) => {
+  let collection = await db.collection('job_postings');
+  let newDocument = req.body;
+
+  if(newDocument.id) {
+    newDocument._id = newDocument.id;
+    delete newDocument.id;
+  }
+  let result = await collection.insertOne(req.body);
+  res.status(201).send(result.ops[0]);
+});
+
+// Update an existing East African country by ID
+router.patch('/:id', async (req, res) => {
+  let collection = await db.collection('job_postings');
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.updateOne(query, { $set: req.body });
+
+  if (result.modifiedCount === 0) res.status(404).send('Not found');
+  else res.status(200).send('Updated successfully');
+});
+
+// Delete an East African country by ID
+router.delete('/:id', async (req, res) => {
+  let collection = await db.collection('job_postings');
+  let query = { _id: Number(req.params.id) };
+  let result = await collection.deleteOne(query);
+
+  if (result.deletedCount === 0) res.status(404).send('Not found');
+  else res.status(204).send(result);
+});
+
+export default router;
